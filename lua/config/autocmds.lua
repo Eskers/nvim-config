fca64a87be857f8e57e2ba23739f0df2e09412c3
Eskers <77648853+Eskers@users.noreply.github.com>
2023-09-17 19:00:04 +0100

    Formatted

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -69,13 +69,13 @@ vim.api.nvim_create_autocmd("FileType", {
 -- Only show cursor line on active buffer
 local cursor_group = vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
 local set_cursorline = function(event, value, pattern)
-  vim.api.nvim_create_autocmd(event, {
-    group = cursor_group,
-    pattern = pattern,
-    callback = function()
-      vim.opt_local.cursorline = value
-    end,
-  })
+    vim.api.nvim_create_autocmd(event, {
+        group = cursor_group,
+        pattern = pattern,
+        callback = function()
+            vim.opt_local.cursorline = value
+      end,
+    })
 end
 set_cursorline("WinLeave", false)
 set_cursorline("WinEnter", true)
