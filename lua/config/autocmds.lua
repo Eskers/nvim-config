3660b65490365c5b8cc10d63338afa1153a99d8c
Eskers <77648853+Eskers@users.noreply.github.com>
2023-09-17 17:42:15 +0100

    Changed formatoptions and only show cursor line on active buffer

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,4 +1,4 @@
--- [[ Highlight on yank ]]
+-- Highlight on yank
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -9,7 +9,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
--- [[ Clear Highlights after search ]]
+-- Clear Highlights after search
 local hl_ns = vim.api.nvim_create_namespace('search')
 local hlsearch_group = vim.api.nvim_create_augroup('hlsearch_group', { clear = true })
 
@@ -35,25 +35,48 @@ vim.api.nvim_create_autocmd('CursorMoved', {
   end,
 })
 
--- [[ Remember last cursor position opening buffer ]]
-local cursor_group = vim.api.nvim_create_augroup("PositionCursor", { clear = true })
+-- Remember last cursor position opening buffer
+local position_group = vim.api.nvim_create_augroup("PositionCursor", { clear = true })
 
 vim.api.nvim_create_autocmd("BufReadPost", {
   command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]],
-  group = cursor_group
+  group = position_group
 })
 
--- [[ Remove trailing whitespace on save ]]
+-- Remove trailing whitespace on save
 local whitespace_group = vim.api.nvim_create_augroup("TrimWhitespace", { clear = true })
 -- end of line
 vim.api.nvim_create_autocmd("BufWritePre", { command = [[:%s/\s\+$//e]], group = whitespace_group, })
 --end of file
 vim.api.nvim_create_autocmd("BufWritePre", { command = [[:%s#\($\n\s*\)\+\%$##e]], group = whitespace_group, })
 
--- [[ Don't autocomment newlines ]]
-local comment_group = vim.api.nvim_create_augroup("NewlineComment", { clear = true })
+-- [[ Required as NVim doesn't seem to respect format options via opt ]]
+local comment_group = vim.api.nvim_create_augroup("FormatOptions", { clear = true })
 
 vim.api.nvim_create_autocmd("FileType", {
-  command = [[set formatoptions-=cro]],
+  command = [[set formatoptions-=o]],
   group = comment_group,
 })
+vim.api.nvim_create_autocmd("FileType", {
+  command = [[set formatoptions-=l]],
+  group = comment_group,
+})
+vim.api.nvim_create_autocmd("FileType", {
+  command = [[set formatoptions+=n]],
+  group = comment_group,
+})
+
+-- Only show cursor line on active buffer
+local cursor_group = vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
+local set_cursorline = function(event, value, pattern)
+  vim.api.nvim_create_autocmd(event, {
+    group = cursor_group,
+    pattern = pattern,
+    callback = function()
+      vim.opt_local.cursorline = value
+    end,
+  })
+end
+set_cursorline("WinLeave", false)
+set_cursorline("WinEnter", true)
+set_cursorline("FileType", false, "TelescopePrompt")
