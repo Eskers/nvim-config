1a43a191cd1181660cca2bf694d1c91dfd87fdca
Eskers <77648853+Eskers@users.noreply.github.com>
2023-09-19 21:49:26 +0100

    Update onedark theme and incc illuminate highlights

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,7 +6,28 @@ return {
 
     "navarasu/onedark.nvim",
     priority = 1000,
+
     config = function()
-        vim.cmd.colorscheme("onedark")
+        require("onedark").setup({
+            style = "warm",
+
+            -- Don't make comments italic
+            code_style = {
+                comments = "none",
+            },
+
+            -- For using vim-illuminate with visual background
+            highlights = {
+                IlluminatedWordText = { bg = "#3b3f4c" },
+                IlluminatedWordRead = { bg = "#3b3f4c" },
+                IlluminatedWordWrite = { bg = "#3b3f4c" },
+            },
+
+            diagnostics = {
+                undercurl = false,
+            }
+        })
+
+        require("onedark").load()
     end,
 }
