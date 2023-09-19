f45969f42977fdb7a5d1195998eb4057c10fb768
Eskers <77648853+Eskers@users.noreply.github.com>
2023-09-19 20:25:50 +0100

    Update treesitter.lua
    Remove treesitter-textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,5 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
     build = ":TSUpdate",
 
     opts = {
@@ -39,49 +36,6 @@ return {
                 node_incremental = "<C-Space>",
                 scope_incremental = "<C-s>",
                 node_decremental = "<M-q>",
-            },
-        },
-        textobjexts = {
-            select = {
-                enable = true,
-                lookahead = true, -- Automatically jump forward to textobj
-                keymaps = {
-                    ["aa"] = "@parameter.outer",
-                    ["ia"] = "@parameter.inner",
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = "@class.inner",
-                },
-            },
-            move = {
-                enable = true,
-                set_jumps = true, -- Whether to set jumps in the jumplist
-                goto_next_start = {
-                    ["]m"] = "@function.outer",
-                    ["]]"] = "@class.outer",
-                },
-                goto_next_end = {
-                    ["]M"] = "@function.outer",
-                    ["]["] = "@class.outer",
-                },
-                goto_previous_start = {
-                    ["[m"] = "@function.outer",
-                    ["[["] = "@class.outer",
-                },
-                goto_previous_end = {
-                    ["[M"] = "@function.outer",
-                    ["[]"] = "@class.outer",
-                },
-            },
-            swap = {
-                enable = true,
-                swap_next = {
-                    ["<leader>a"] = "@parameter.inner",
-                },
-                swap_previous = {
-                    ["<leader>A"] = "@parameter.inner",
-                },
             },
         },
     },
