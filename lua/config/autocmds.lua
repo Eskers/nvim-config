edff94c19dff1390f91b2ceb802d4b4acb6c9c7d
Eskers <77648853+Eskers@users.noreply.github.com>
2023-09-20 19:05:22 +0100

    Close certain filetypes with q

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -80,3 +80,28 @@ end
 set_cursorline("WinLeave", false)
 set_cursorline("WinEnter", true)
 set_cursorline("FileType", false, "TelescopePrompt")
+
+-- close some filetypes with <q>
+local close_group = vim.api.nvim_create_augroup("CloseGroup", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = close_group,
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "man",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+    "neotest-output",
+    "checkhealth",
+    "neotest-summary",
+    "neotest-output-panel",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
